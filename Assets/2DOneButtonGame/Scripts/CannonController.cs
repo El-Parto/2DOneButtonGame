1136207fb98cfb01e290ac9575a5d43d446e57b9
Luc <luc980@y7mail.com>
2021-03-31 17:59:50 +1100

    StartingCannonControl

--- Assets/2DOneButtonGame/Scripts/CannonController.cs
+++ Assets/2DOneButtonGame/Scripts/CannonController.cs
@@ -2,8 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Cannon script. This script is planned to handle the following;
+/// * Upon player colliding with self, turn off mesh render for player (entering cannon)
+/// * Keep player static when inside cannon
+/// * on input of button fire cannon. However firing cannon can only happen
+/// when inside a cannon (exiting cannon.)
+/// 
+/// * Keep the game from thinking that we're in every cannon that is able to fire
+/// Basically, have each cannon "active" when player is inside cannon, 
+/// but "inactive" when player is outside cannon.
+/// 
+/// 
+///
+/// </summary>
+
+
+
+
 public class CannonController : MonoBehaviour
 {
+    public GameObject Cannon;
+    public bool inCannon;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +36,26 @@ public class CannonController : MonoBehaviour
     {
         
     }
+
+    public void CannonEnter()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+    }
+
+
+
+
+
+
+
 }
--- Assets/2DOneButtonGame/Scripts/PlayerScript.cs
+++ Assets/2DOneButtonGame/Scripts/PlayerScript.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// The script that controls the player. The purpose of this script is planned to have;
+/// 
+/// (While player isn't able to move on their own, they are able to control when they fire out of the cannon)
+/// * a check to whether player is inside cannon
+/// * A check to see if touching the victory "platform" (platform is a place holder)
+/// * A check to see if the player is touching the death barrier 
+/// * If the player is touching this death barrier, the player is destroyed
+/// * When player is destroyed, lose a life.
+/// *
+/// 
+/// </summary>
 public class PlayerScript : MonoBehaviour
 {
-    private Rigidbody2D playerRB ; 
+    private Rigidbody2D playerRB;
+    private GameObject player;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRB = playerRB.GetComponent<Rigidbody2D>();
+        playerRB = gameObject.GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
