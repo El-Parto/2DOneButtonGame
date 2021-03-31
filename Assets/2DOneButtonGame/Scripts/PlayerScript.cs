affd8b769155e01dee31c7321ed1c50ee10d8e4b
Luc <luc980@y7mail.com>
2021-03-31 17:30:17 +1100

    31stmarchupdate

---
+++ Assets/2DOneButtonGame/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D playerRB ; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = playerRB.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
